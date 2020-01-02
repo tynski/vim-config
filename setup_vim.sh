81aa3ab1dad681407163897215a3e368b887b2c4
tynski <b.tynski@gmail.com>
2020-01-02 13:10:03 +0100

    setup vim update

--- setup_vim.sh
+++ setup_vim.sh
@@ -4,5 +4,3 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./vimrc ~/.vim/vimrc
 ln ~/.vimrc ~/.vim/vimrc
 vim +PluginInstall +qall
-
-echo "Dont forget to install / update YoucompleteMe!"
